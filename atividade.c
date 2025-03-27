240e03d6e36d8805497def40016674f8edbd1dc5
Escandiel2702 <nicolassantosescandiel@gmail.com>
2025-03-26 21:40:37 -0300

    Add files via upload

---
+++ atividade.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define VETOR 100
+int main(){
+
+int vetor [VETOR];
+ vetor [0]=10;
+ vetor [1]=46;
+ vetor [2]=41;
+ vetor [3]=60;
+
+
+vetor [4]=200;
+printf("valor do vetor:\n");
+
+for(int i =0; i<4;i++){
+printf("vetor[%d]= %d\n",i,vetor [i]);
+
+}
+
+
+
+
+
+
+
+
+}
